5f8911735300c55ab1e75995bd525ef066e1aa08
alieliwa <alieliwa515@gmail.com>
2025-04-07 11:16:53 +0200

    navigate home & refactor init

--- lib/constans.dart
+++ lib/constans.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/cupertino.dart';
 
 const primaryColor = Color(0xff100B20);
+const transationDuration = Duration(microseconds: 250);
--- lib/features/Splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/Splash/presentation/views/widgets/splash_view_body.dart
@@ -1,6 +1,10 @@
+import 'package:bookly_app_clean_arch/constans.dart';
 import 'package:bookly_app_clean_arch/core/utils/assets_data.dart';
 import 'package:bookly_app_clean_arch/features/Splash/presentation/views/widgets/sliding_text.dart';
+import 'package:bookly_app_clean_arch/features/home/presentaion/views/home_view.dart';
 import 'package:flutter/material.dart';
+import 'package:get/instance_manager.dart';
+import 'package:get/route_manager.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -17,15 +21,8 @@ class _SplashViewBodyState extends State<SplashViewBody>
   @override
   void initState() {
     super.initState();
-    animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 1),
-    );
-    slidingAnimation = Tween<Offset>(
-      begin: Offset(0, 5),
-      end: Offset.zero,
-    ).animate(animationController);
-    animationController.forward();
+    slidingAnimtaionMethode();
+    navigateToHome();
   }
 
   @override
@@ -45,5 +42,27 @@ class _SplashViewBodyState extends State<SplashViewBody>
         SlidingText(slidingAnimation: slidingAnimation),
       ],
     );
+  }
+
+  void slidingAnimtaionMethode() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    slidingAnimation = Tween<Offset>(
+      begin: Offset(0, 5),
+      end: Offset.zero,
+    ).animate(animationController);
+    animationController.forward();
+  }
+
+  void navigateToHome() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Get.to(
+        () => HomeView(),
+        transition: Transition.fade,
+        duration: transationDuration,
+      );
+    });
   }
 }
